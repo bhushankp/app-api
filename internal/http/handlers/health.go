22a7c438413f4351ad0ac7d401b61adb83933793
Bhushanpatil <bhushanpatilbkp68@gmail.com>
2025-08-15 13:56:34 +0530

    Updated healht checking

--- internal/http/handlers/health.go
+++ internal/http/handlers/health.go
@@ -2,24 +2,13 @@ package handlers
 
 import (
 	"net/http"
-	"time"
 
 	"github.com/bhushankp/app-api.git/internal/pkg/respond"
 )
 
-var (
-	Version   = "dev"
-	Commit    = "none"
-	BuildTime = "unknown"
-)
-
 func Health(w http.ResponseWriter, r *http.Request) {
-	resp := map[string]interface{}{
-		"status":     "ok",
-		"version":    Version,
-		"commit":     Commit,
-		"build_time": BuildTime,
-		"ts":         time.Now().UTC().Format(time.RFC3339),
-	}
-	respond.JSON(w, http.StatusOK, resp)
+	respond.JSON(w, http.StatusOK, map[string]string{
+		"status":  "ok",
+		"message": "service is alive",
+	})
 }
